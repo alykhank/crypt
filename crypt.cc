568f99768b9d54c5e14aa075af6f321424572db0
Alykhan Kanji <a.kanji@me.com>
2012-02-24 13:05:01 -0500

    Added input prompt, output user-defined phrase

--- crypt.cc
+++ crypt.cc
@@ -6,5 +6,8 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << "Cryptography is my passion." << endl;
+    string s;
+    cout << "Please enter a phrase: ";
+    cin >> s;
+    cout << "Cryptography is my passion:" << endl << s << endl;
 }
